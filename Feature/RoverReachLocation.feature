54b486e9b4cc3e554c67bd3f2c91d94e9b1c76c4
Christoph Kuhn <Christoph Kuhn@LMSWET-H0VB0F3>
2022-04-04 07:49:06 +0200

    Add project files.

---
+++ Feature/RoverReachLocation.feature
@@ -0,0 +1,17 @@
+﻿Feature: RoverReachLocation
+
+Rover should be successfully able to reach required location.
+
+@tag1
+Scenario Outline: Reach destination
+Given Rover is in plateau and operated
+When the rover accepts First line of input
+| <X>       |
+| <y>       |
+| <Direction> |
+And Second line of <Second input>
+Then the rover reaches desired location <Location>
+Examples: 
+| X | y | Direction |Second input  | Location |
+| 1 | 2 | N         | LMLMLMLMM	   | 1 3 N    |
+| 3 | 3 | E         | MMRMMRMRRM   | 5 1 E    |
---
+++ Steps/TurnRoverSteps.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace MarsRovers.Steps
+{
+    [Binding]
+    public class TurnRoverSteps
+    {
+        private readonly ScenarioContext _scenarioContext;
+        private Rover rover;
+
+        public TurnRoverSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"Rover is in plateau at \((\d+),(\d+)\) co-ordinates and facing ([NEWS]{1})")]
+        public void GivenRoverIsInPlateauAtCo_OrdinatesAndFacing(int x, int y, string direction)
+        {
+            rover = new Rover();
+            Point point = new Point(x,y);
+            rover.Position = point;
+            rover.Direction = direction;
+        }
+
+        [When(@"the rover turns ([LR]{1})")]
+        public void WhenTheRoverTurns(string turnDirection)
+        {
+            rover.Turn(turnDirection);
+        }
+
+        [Then(@"Rover turns to a new direction ([NEWS]{1}) with same cordinates \((\d+),(\d+)\)")]
+        public void ThenRoverTurnsToANewDirectionWithSameCordinates(string direction,int x, int y)
+        {
+            rover.Direction.Should().Be(direction);
+            rover.Position.Should().Be(new Point(x, y));
+        }
+    }
+}
